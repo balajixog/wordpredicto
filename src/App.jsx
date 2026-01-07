7251818540ec34f7bb6d1786e8f7a16bfe6b03e3
Balaji G <balaji31072005@gmail.com>
2026-01-07 15:34:47 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -162,32 +162,6 @@ useEffect(() => {
   }
 }, [isGameLost]);
 
-//brust confetti
-//   useEffect(() => {
-//   if (!isGameWon) return;
-
-//   const duration = 2 * 1000;
-//   const end = Date.now() + duration;
-
-//   (function frame() {
-//     confetti({
-//       particleCount: 5,
-//       angle: 60,
-//       spread: 55,
-//       origin: { x: 0 }
-//     });
-//     confetti({
-//       particleCount: 5,
-//       angle: 120,
-//       spread: 55,
-//       origin: { x: 1 }
-//     });
-
-//     if (Date.now() < end) {
-//       requestAnimationFrame(frame);
-//     }
-//   })();
-// }, [isGameWon]);
 
 
   return(
